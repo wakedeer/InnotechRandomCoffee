0b4eac38c22ef645a256f5a4071d7577cc669069
Миронов Алексей Юрьевич (4052206) <aumironov@vtb.ru>
2022-05-03 17:20:26 +0300

    fix

--- src/main/kotlin/inno/tech/handler/state/ReadyHandler.kt
+++ src/main/kotlin/inno/tech/handler/state/ReadyHandler.kt
@@ -7,10 +7,14 @@ import inno.tech.constant.Status
 import inno.tech.exception.RandomCoffeeBotException
 import inno.tech.handler.Handler
 import inno.tech.model.User
+import inno.tech.repository.UserRepository
+import inno.tech.service.SubscriptionService
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import java.time.DayOfWeek
+import java.time.LocalDateTime
 
 /**
  * Обработчик сообщения о готовности участвовать в жеребьёвке.
@@ -20,6 +24,8 @@ import org.telegram.telegrambots.meta.api.objects.Update
 @Component
 class ReadyHandler(
     private val telegramBotApi: TelegramBotApi,
+    private val userRepository: UserRepository,
+    private val subscriptionService: SubscriptionService,
 ) : Handler {
 
     override fun accept(command: String, user: User?): Boolean {
@@ -31,12 +37,23 @@ class ReadyHandler(
             throw RandomCoffeeBotException("user cannot be null")
         }
 
-        user.status = Status.READY
+        val now = LocalDateTime.now()
+        val dayOfWeek = now.dayOfWeek
+        val hour = now.hour
+        val fastMatchingTime = DayOfWeek.FRIDAY <= dayOfWeek && 7 <= hour || DayOfWeek.SUNDAY >= dayOfWeek && 7 >= hour
+        val readyUser = userRepository.findAllByStatusAndActiveTrue(Status.READY).firstOrNull()
+        if (fastMatchingTime && readyUser != null) {
+            //fast matching
+            subscriptionService.sendInvitation(readyUser, user)
+        } else {
+            //just change status
+            user.status = Status.READY
 
-        val pauseReply = SendMessage()
-        pauseReply.text = Message.READY_TO_MATCH
-        pauseReply.parseMode = ParseMode.MARKDOWN
-        pauseReply.chatId = user.chatId.toString()
-        telegramBotApi.execute(pauseReply)
+            val pauseReply = SendMessage()
+            pauseReply.text = Message.READY_TO_MATCH
+            pauseReply.parseMode = ParseMode.MARKDOWN
+            pauseReply.chatId = user.chatId.toString()
+            telegramBotApi.execute(pauseReply)
+        }
     }
 }
